13589b8981d3e4d120221602a35675f56de2c563
XiaoNaTongzhi <113130607+XiaoNaTongzhi@users.noreply.github.com>
2023-03-09 20:49:46 +0300

    Create Bike.java

---
+++ Bike.java
@@ -0,0 +1,15 @@
+public class Bike extends Vehicles{
+
+    boolean motor;
+    boolean pedals;
+
+    public Bike(int wheels, boolean canFly, int seats, double maxSpeed, double minSpeed, boolean motor, boolean pedals) {
+        super(wheels, canFly, seats, maxSpeed, minSpeed);
+        this.motor = motor;
+        this.pedals = pedals;
+    }
+
+    
+    
+} 
+    
