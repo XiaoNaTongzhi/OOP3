993ace661e302fdde59c68234424c209fb4085ee
XiaoNaTongzhi <113130607+XiaoNaTongzhi@users.noreply.github.com>
2023-03-09 21:40:36 +0300

    Update and rename CarMain.java to Main.java

---
+++ Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        
+        Moped mop1 = new Moped(2, false, 2, 80, 0, true, true);
+        System.out.println(mop1);
+        
+        // Car car1 = new Car(4, false, 4, 200, 0, "Жигули", 1994, 30, "Жёлтый", 0, false);
+
+        // car1.GetInfo();
+
+        // System.out.println("Старая цена: " + car1.GetPrice());
+        // car1.SetPrice(30.5);
+        // System.out.println("Новая цена: " + car1.GetPrice());
+
+        // car1.driveTo("Питер");
+        // car1.powerOn();
+        // car1.driveTo("Тбилиси");
+
+    }
+}
+
