a5e71f2d0b5eee0ef7ac3194fce7315d87fbfc6e
XiaoNaTongzhi <113130607+XiaoNaTongzhi@users.noreply.github.com>
2023-03-09 20:53:51 +0300

    Create Moped.java

---
+++ Moped.java
@@ -0,0 +1,11 @@
+public class Moped extends Bike{
+
+    public Moped (int wheels, boolean canFly, int seats, double maxSpeed, double minSpeed, boolean motor,
+            boolean pedals) {
+        super(wheels, canFly, seats, maxSpeed, minSpeed, motor, pedals);
+        
+    }
+
+    
+    
+}
