b14c0a446e92b8a56582111cabfdb7b31fb97354
XiaoNaTongzhi <113130607+XiaoNaTongzhi@users.noreply.github.com>
2023-03-09 21:48:53 +0300

    Update Bike.java

--- Bike.java
+++ Bike.java
@@ -1,18 +1,38 @@
 public class Bike extends Vehicles{
 
-    boolean motor;
-    boolean pedals;
+    private boolean motor;
+    private boolean pedals;
 
     public Bike(int wheels, boolean canFly, int seats, double maxSpeed, double minSpeed, boolean motor, boolean pedals) {
         super(wheels, canFly, seats, maxSpeed, minSpeed);
-        this.motor = motor;
+        this.setMotor(motor);
+        this.setPedals(pedals);
+    }
+
+
+    public boolean isPedals() {
+        return pedals;
+    }
+
+
+    public void setPedals(boolean pedals) {
         this.pedals = pedals;
+    }
+
+
+    public boolean isMotor() {
+        return motor;
+    }
+
+
+    public void setMotor(boolean motor) {
+        this.motor = motor;
     }
 
 
     @Override
     public String toString() {
-        return "Bike [Мотор: " + motor + ", Педали: " + pedals + "]";
+        return "Bike [Мотор: " + isMotor() + ", Педали: " + isPedals() + "]";
     }
 } 
     
